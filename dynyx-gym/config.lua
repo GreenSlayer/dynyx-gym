69430fa8c604045ff0d753cc6a2030dce4b6305a
Green <80470427+GreenSlayer@users.noreply.github.com>
2024-08-14 13:38:15 -0400

    Update config.lua
    # Update / Bug Fixes
    
    - Supports OX
    - Code Cleanup
    - Addeed new minigame
    - Fixed Threadmills
    - Fixed Yoga Mat
    - Fixed Target Eye
    - and More Bug Fixing

--- dynyx-gym/config.lua
+++ dynyx-gym/config.lua
@@ -1,33 +1,37 @@
 -- |  https://discord.com/invite/A4gVRjnvaE    | For any support 
 
---Make sure you have this https://github.com/GreenSlayer/mz-skills
+-- Make sure you have this https://github.com/GreenSlayer/mz-skills
+
+-- ***IF YOU ARE USING OX UNCOMMENT "@ox_lib/init.lua", IN fxmanifest.lua***
+
+Config = {}
+
+Config.Target = 'qb' -- ox / qb
+Config.Menu = 'qb' -- ox / qb
+Config.Progressbar = 'qb' -- ox / qb
+Config.Inventory = 'qb' -- ox / qb
+Config.Notifications = 'qb' -- ox / qb
+
+Config.GymPed = "a_m_y_clubcust_04" --  Ped Model of the Ped you buy a membership
+Config.GymPedSpawn = vector4(-1255.53, -354.77, 35.96, 296.64) -- Location of the Ped you buy membership and Blip Location
+Config.BlipName = 'Gym' -- Blip Name
+
+Config.GymPassPrice = 1250 --  Price of Membership
+Config.GymPassItem = 'gym_pass' -- Item Name of Gym Membership
+
+Config.EnableMinigame = true -- (true == Enables Minigame) (false == Disables Minigame)
+Config.Minigame = 'skillCheck' -- qb-lock / ps-ui / skillCheck (Ox_lib Required)(If using skillcheck, Minigame setting wont work)
 
 
-config = {}
-
-config.GymNPC = {
-    pedname = "a_m_y_clubcust_04", -- Name of the Ped
-    pedhash = 0xE2210515, -- Hash Code for the Ped
-    pedspawn = vector4(-1255.53, -354.77, 35.96, 296.64) --  Spawn for the Gym Membership Seller NPC
-}
-
-config.GymPass = {
-    Price = 1250, -- Price for the Gym Membership
-    item = "gym_pass" -- The Item Name for the Gym Membership
-}
-
-config.Minigame = 'ps-ui' -- qb-lock / ps-ui 
-
-
-config.Skills = {
+Config.Skills = {
     ['ThreadMills'] = {
         skill = 'Stamina', -- The Type of Skill
         amount = math.random(0, 1),  -- The amount of skill rep you gain
         Stress = math.random(5, 9), -- GainStress -- You Can set the amount of Stress you gain here
         ProgressbarDuration = math.random(1000, 2500), -- This is the duration for the progressbar
         Minigame = { -- Circle Minigame
-            time = 23,
-            circles = 8,
+            time = 5,
+            circles = 1,
             FailedMinigameStress = math.random(10, 15) -- If you failed the minigame you will gain stress as a punishment for failing it
         }
     },
@@ -40,7 +44,9 @@ config.Skills = {
             time = 20,
             circles = 7,
             FailedMinigameStress = math.random(10, 15) -- If you failed the minigame you will gain stress as a punishment for failing it
-        }
+        }, 
+        ChinupAnimCoords = vector4(-1258.79, -355.49, 35.96, 338.63),
+        ChinupAnimCoords2 = vector4(-1257.45, -358.75, 35.96, 294.17)
     },
     ['LiftWeights'] = {
         skill = 'Strength', -- The Type of Skill
@@ -66,8 +72,7 @@ config.Skills = {
     },
 }
 
--- Dont Touch the following
-config.Threadmills = {
+Config.Threadmills = { -- Coords of each Threadmill
     [1] = {
         coords = vector3(-1257.55, -366.77, 36.96)
     },
@@ -85,5 +90,14 @@ config.Threadmills = {
     },
     [6] = {
         coords = vector3(-1264.67, -370.58, 36.96)
+    },
+}
+
+Config.LiftWeight = { -- Coords of each Weight
+    [1] = {
+        coords = vector3(-1268.11, -366.05, 36.99)
+    },
+    [2] = {
+        coords = vector3(-1269.8, -362.56, 36.96)
     },
 }
