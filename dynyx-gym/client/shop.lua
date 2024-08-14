9f97b65ae8d0b7793dcc67d33db649aab0218842
Green <80470427+GreenSlayer@users.noreply.github.com>
2024-08-14 13:37:41 -0400

    Update shop.lua
    # Update / Bug Fixes
    
    - Supports OX
    - Code Cleanup
    - Addeed new minigame
    - Fixed Threadmills
    - Fixed Yoga Mat
    - Fixed Target Eye
    - and More Bug Fixing

--- dynyx-gym/client/shop.lua
+++ dynyx-gym/client/shop.lua
@@ -1,115 +1,176 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 CreateThread(function()
-    exports['qb-target']:AddBoxZone("gymnpc", config.GymNPC.pedspawn, 1.5, 1.5, {
-        name = "gymnpc",
-        heading = 0,
-        debugPoly = false,
-        minZ = 30.0,
-        maxZ = 39.0,
-      }, {
-        options = {
-          {
-            type = "client",
-            event = "dynyx-gym:OpenMemberBuy",
-            icon = 'fas fa-card',
-            label = 'Buy Membership'
-          },
-        },
-        distance = 2.0
-        })
-
-end)
-
-RegisterNetEvent('dynyx-gym:OpenMemberBuy', function()
-    exports['qb-menu']:openMenu({
-        {
-            id = 1,
-            header = "Welcome! Please Purchase a Gym Member to workout!",
-        },
-        {
-          id = 2,
-          header = "Buy Gym Membership",
-          txt = "$"..config.GymPass.Price,
-          params = {
-              event = "dynyx-gym:LifetimeConfirm",
-          }
-      },
-    })
-end)
-
-RegisterNetEvent('dynyx-gym:LifetimeConfirm', function()
-    exports['qb-menu']:openMenu({
-        {
-          id = 1,
-          header = "Go Back",
-          params = {
-              event = "dynyx-gym:OpenMemberBuy",
-          }
-      },
-      {
-        id = 2,
-        header = "Confirm Purchase",
-        params = {
-            event = "dynyx-gym:client:Start",
-        }
-    },
-    })
-end)
-
-
-CreateThread(function()
-    RequestModel( GetHashKey( config.GymNPC.pedname ) )
-    while ( not HasModelLoaded( GetHashKey( config.GymNPC.pedname ) ) ) do
-        Citizen.Wait( 1 )
+    -- Makes Blip
+    Gym = AddBlipForCoord(Config.GymPedSpawn)
+    SetBlipSprite(Gym, 311)
+    SetBlipDisplay(Gym, 4)
+    SetBlipScale(Gym, 0.65)
+    SetBlipAsShortRange(Gym, true)
+    SetBlipColour(Gym, 7)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(Config.BlipName)
+    EndTextCommandSetBlipName(Gym)
+    -- Spawns Ped
+    local PedCoords = Config.GymPedSpawn
+    PedHash = GetHashKey(Config.GymPed)
+    RequestModel(PedHash)
+    while not HasModelLoaded(PedHash) do
+        Citizen.Wait(1)
     end
-    gymnpc = CreatePed(1, config.GymNPC.pedhash, config.GymNPC.pedspawn, false, true)
-    FreezeEntityPosition(gymnpc, true)
-    SetEntityInvincible(gymnpc, true)
-    SetBlockingOfNonTemporaryEvents(gymnpc, true)
-    TaskStartScenarioInPlace(gymnpc, 'WORLD_HUMAN_CLIPBOARD', 0, true)
+    if HasModelLoaded(PedHash) then
+        local GymPed = CreatePed(1, PedHash, PedCoords.x, PedCoords.y, PedCoords.z, PedCoords.w, false, true)
+        FreezeEntityPosition(GymPed, true)
+        SetEntityInvincible(GymPed, true)
+        TaskStartScenarioInPlace(GymPed, "WORLD_HUMAN_CLIPBOARD", 0, true)
+        SetBlockingOfNonTemporaryEvents(GymPed, true)
+        if Config.Target == 'qb' then
+          exports['qb-target']:AddBoxZone("gymnpc", vector3(PedCoords.x, PedCoords.y, PedCoords.z + 1), 1.5, 1.5, {
+            name = "gymnpc",
+            heading = 0,
+            debugPoly = false,
+            minZ = 30.0,
+            maxZ = 39.0,
+          }, {
+            options = {
+              {
+                type = "client",
+                event = "dynyx_gym:OpenMemberBuy",
+                icon = 'fa-solid fa-book',
+                label = 'Buy Membership'
+              },
+            },
+            distance = 2.0
+          })
+        elseif Config.Target == 'ox' then
+          exports.ox_target:addSphereZone({
+            coords = vec3(PedCoords.x, PedCoords.y, PedCoords.z + 1),
+            radius = 1,
+            debug = false,
+            options = {
+                {
+                    onSelect = function()
+                        TriggerEvent('dynyx_gym:OpenMemberBuy')
+                    end,
+                    icon = 'fa-solid fa-book',
+                    label = "Buy Membership",
+                }
+            }
+          })
+        else
+          print("**Invalid Config.Target.**")
+        end
+    end
 end)
 
-CreateThread(function()
-  MakeBlips()
+RegisterNetEvent('dynyx_gym:OpenMemberBuy', function()
+    if Config.Menu == 'qb' then
+        exports['qb-menu']:openMenu({
+            {
+                id = 1,
+                header = "Welcome! Please Purchase a Gym Member to workout!",
+            },{
+              id = 2,
+              header = "Buy Gym Membership",
+              txt = "$"..Config.GymPassPrice,
+              params = {
+                  event = "dynyx_gym:LifetimeConfirm",
+              }
+          },
+        })
+    elseif Config.Menu == 'ox' then
+        lib.registerContext({
+            id = 'GymMemberMenu1',
+            title = 'Welcome! Please Purchase a Gym Membership to workout!',
+            options = {
+              {
+                title = 'Buy Gym Membership',
+                description = '$'..Config.GymPassPrice,
+                arrow = true,
+                icon = 'dollar',
+                event = 'dynyx_gym:LifetimeConfirm',
+              }
+            }
+        })
+        lib.showContext('GymMemberMenu1')
+    else
+        print("**Invalid Config.Menu.**")
+    end
 end)
 
-function MakeBlips()
-  Gym = AddBlipForCoord(vector3(-1262.78, -360.72, 36.99))
-  SetBlipSprite (Gym, 311)
-  SetBlipDisplay(Gym, 4)
-  SetBlipScale  (Gym, 0.65)
-  SetBlipAsShortRange(Gym, true)
-  SetBlipColour(Gym, 7)
-  BeginTextCommandSetBlipName("STRING")
-  AddTextComponentSubstringPlayerName('Gym')
-  EndTextCommandSetBlipName(Gym)
-end  
+RegisterNetEvent('dynyx_gym:LifetimeConfirm', function()
+    if Config.Menu == 'qb' then
+        exports['qb-menu']:openMenu({
+            {
+                id = 1,
+                header = "Go Back",
+                params = {
+                    event = "dynyx-gym:OpenMemberBuy",
+                }
+            },{
+                id = 2,
+                header = "Confirm Purchase",
+                txt = "Purchase of $"..Config.GymPassPrice,
+                params = {
+                    event = "dynyx_gym:BuyCard",
+                }
+            },
+        })
+    elseif Config.Menu == 'ox' then
+        lib.registerContext({
+            id = 'GymMemberMenu2',
+            title = 'Please Confirm.',
+            options = {
+              {
+                title = 'Confirm Purchase',
+                description = 'Purchase of $'..Config.GymPassPrice,
+                icon = 'dollar',
+                event = 'dynyx_gym:BuyCard',
+              }
+            }
+        })
+        lib.showContext('GymMemberMenu2')
+    else
+        print("**Invalid Config.Menu.**")
+    end
+end)
 
-local function GivePass(bool, price, item)
-  TriggerServerEvent('dynyx-gym:server:GivePass', price, item)
-end
-
-
-
-RegisterNetEvent("dynyx-gym:client:Start", function(data)
-  local Item = config.GymPass.item
-  local Price = config.GymPass.Price
-
-  QBCore.Functions.TriggerCallback('dynyx-gym:server:CheckMoney', function(money)
-      if money then
+RegisterNetEvent('dynyx_gym:BuyCard', function()
+    if Config.Progressbar == 'qb' then
         QBCore.Functions.Progressbar('random_task', 'Processing Purchase', 3000, false, false, {
-          disableMovement = true,
-          disableCarMovement = true,
-          disableMouse = false,
-          disableCombat = true,
-        }, {
-          animDict = "missheistdockssetup1clipboard@base",
-          anim = "base",
-          flags = 8,
-        }, {}, {}, function()  
-          GivePass(true, Price, Item)
-          end)
-      end
-  end, Price)
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+          }, {
+            animDict = "missheistdockssetup1clipboard@base",
+            anim = "base",
+            flags = 8,
+          }, {}, {}, function()  
+              TriggerServerEvent('dynyx_gym:BuyGymM')
+        end)
+    elseif Config.Progressbar == 'ox' then
+        exports.ox_inventory:Progress({
+            duration = 3000,
+            label = "Processing Purchase",
+            useWhileDead = false,
+            disable = {
+                move = true,
+                car = true,
+                combat = true,
+                mouse = false,
+            },
+            anim = {
+                dict = "missheistdockssetup1clipboard@base",
+                clip = "base",
+                flags = 8,
+            },
+        }, function(cancel)
+            if not cancel then
+                TriggerServerEvent('dynyx_gym:BuyGymM')
+            end
+        end)
+    else
+        print("**Invalid Config.Progressbar.**")
+    end
 end)
