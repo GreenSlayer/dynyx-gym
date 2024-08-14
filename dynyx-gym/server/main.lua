9b5d7784eeb00f6cda8f54ff441b983ad722aa9c
Green <80470427+GreenSlayer@users.noreply.github.com>
2024-08-14 13:37:59 -0400

    Update main.lua
    # Update / Bug Fixes
    
    - Supports OX
    - Code Cleanup
    - Addeed new minigame
    - Fixed Threadmills
    - Fixed Yoga Mat
    - Fixed Target Eye
    - and More Bug Fixing

--- dynyx-gym/server/main.lua
+++ dynyx-gym/server/main.lua
@@ -1,26 +1,39 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-
-RegisterServerEvent('dynyx-gym:server:GivePass', function(price, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-
-    Player.Functions.RemoveMoney('cash', price)
-    Player.Functions.AddItem(item, 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], "add")
-end)
-
-QBCore.Functions.CreateCallback('dynyx-gym:server:CheckMoney',function(source, cb, price)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local CashAmount = Player.Functions.GetMoney('cash')
-    local callback = false
-
-    if CashAmount >= price then
-        callback = true
+function Notify(source, text, status)
+    if Config.Notifications == "ox" then
+        TriggerClientEvent('ox_lib:notify', source, {
+            title = 'GYM',
+            description = text,
+            type = status
+        })
+    elseif Config.Notifications == "qb" then
+        TriggerClientEvent('QBCore:Notify', source, text, status)
     else
-        TriggerClientEvent('QBCore:Notify', src, 'Not Enough Money', 'error')
+        print("Config.Notifications is invalid.")
     end
+end
+RegisterServerEvent('dynyx_gym:BuyGymM', function()
+    local src = source
+    if Config.Inventory == 'qb' then
+        local Player = QBCore.Functions.GetPlayer(src)
+        local cashcurr = Player.Functions.GetMoney('cash')
 
-    cb(callback)
+        if cashcurr >= Config.GymPassPrice then
+            Player.Functions.RemoveMoney('cash', Config.GymPassPrice)
+            Player.Functions.AddItem(Config.GymPassItem, 1)
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.GymPassItem], "add")
+        else
+            Notify(src, 'Not Enough Money', 'error')
+        end
+    elseif Config.Inventory == 'ox' then
+        local item = exports.ox_inventory:GetItem(src, "money")
+        
+        if item.count >= Config.GymPassPrice then
+            exports.ox_inventory:RemoveItem(src, "money", Config.GymPassPrice)
+            exports.ox_inventory:AddItem(src, Config.GymPassItem, 1)
+        else
+            Notify(src, 'Not Enough Money', 'error')
+        end
+    end
 end)
